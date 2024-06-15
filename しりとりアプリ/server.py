c412ca9ad468e55a422d5563217f6fd0d5837325
Taiyo Kanamori <Tahiyau000@gmail.com>
2024-06-15 18:40:01 +0900

    Add files via upload

---
+++ しりとりアプリ/server.py
@@ -0,0 +1,53 @@
+from flask import Flask, send_from_directory, request, render_template
+
+app = Flask(__name__, template_folder='public')
+
+previous_word = "しりとり"
+words = [] 
+
+@app.route('/')
+def index():
+    previous_word = "しりとり"
+    return render_template('index.html', previous_word=previous_word)
+
+@app.route('/reset')
+def reset():
+    global previous_word
+    global words
+    previous_word  = "しりとり"
+    words = []
+    return render_template('index.html', previous_word=previous_word)
+
+@app.route('/enter', methods=['POST'])
+def enter():
+    global previous_word
+    global words
+    enter_word = request.form['word']
+
+    if enter_word == '' :
+        miss = "前の単語に続いていません"
+        return render_template('index.html', previous_word  = previous_word, miss=miss, word=enter_word)
+    
+    pre_end_value = previous_word[-1]
+    ent_head_value = enter_word[0]
+
+    if pre_end_value == ent_head_value:
+        miss = ''
+
+        if enter_word[-1] == "ん" :
+            return render_template('end.html', word = enter_word)
+        
+        elif enter_word in words:
+            return render_template('same_word.html', word = enter_word)
+        
+        else :
+            previous_word = enter_word
+            words.append(previous_word)
+            return render_template('index.html', previous_word=previous_word, miss=miss)
+        
+    else :
+        miss = "前の単語に続いていません"
+        return render_template('index.html', previous_word=previous_word, miss=miss, word=enter_word)
+
+if __name__ == '__main__' :
+    app.run(debug=True, port=8000)
