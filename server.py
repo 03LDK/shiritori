776ecf76ec3a983ff5db792cd659dcb036af0da9
Taiyo Kanamori <Tahiyau000@gmail.com>
2024-06-18 21:41:05 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -4,11 +4,13 @@ app = Flask(__name__, template_folder='public')
 
 previous_word = "しりとり"
 words = [] 
+start_word = "しりとり"
+len_words = 0
 
 @app.route('/')
 def index():
     previous_word = "しりとり"
-    return render_template('index.html', previous_word=previous_word)
+    return render_template('index.html', previous_word=previous_word, start_word=start_word)
 
 @app.route('/reset')
 def reset():
@@ -16,17 +18,19 @@ def reset():
     global words
     previous_word  = "しりとり"
     words = []
-    return render_template('index.html', previous_word=previous_word)
+    miss = ''
+    return render_template('index.html', previous_word=previous_word, miss=miss, start_word=start_word, )
 
 @app.route('/enter', methods=['POST'])
 def enter():
     global previous_word
     global words
+    global len_words
     enter_word = request.form['word']
 
     if enter_word == '' :
         miss = "前の単語に続いていません"
-        return render_template('index.html', previous_word  = previous_word, miss=miss, word=enter_word)
+        return render_template('index.html', previous_word=previous_word, miss=miss, word=enter_word, words=words, start_word=start_word)
     
     pre_end_value = previous_word[-1]
     ent_head_value = enter_word[0]
@@ -35,19 +39,20 @@ def enter():
         miss = ''
 
         if enter_word[-1] == "ん" :
-            return render_template('end.html', word = enter_word)
+            return render_template('end.html', word = enter_word, len_words=len_words, words=words, start_word=start_word)
         
         elif enter_word in words:
-            return render_template('same_word.html', word = enter_word)
+            return render_template('same_word.html', word = enter_word, len_words=len_words, words=words, start_word=start_word)
         
         else :
             previous_word = enter_word
             words.append(previous_word)
-            return render_template('index.html', previous_word=previous_word, miss=miss)
+            len_words = len(words)
+            return render_template('index.html', previous_word=previous_word, miss=miss, words=words, start_word=start_word)
         
     else :
         miss = "前の単語に続いていません"
-        return render_template('index.html', previous_word=previous_word, miss=miss, word=enter_word)
+        return render_template('index.html', previous_word=previous_word, miss=miss, word=enter_word, words=words, start_word=start_word)
 
 if __name__ == '__main__' :
     app.run(debug=True, port=8000)
